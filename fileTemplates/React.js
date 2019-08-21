dc12165555bad83084b2a9f5de399c51dbbc3ff7
Leszek Rutkowski <l.rutkowski@bioinfo.pl>
2019-08-21 10:24:47 +0200

    WS-2019.1.3 Create baseRefactoring.xml, markdown.xml, ide.general.xml, project.default.xml, diff.xml, React.js, user.xml, Zen CSS.xml, terminal.xml, Default.xml, file.template.settings.xml, find.xml, ui.lnf.xml, filetypes.xml, plugin_ui.xml, colors.scheme.xml, _@user_Material Oceanic.icls, _@user_Darcula.icls, Zen HTML.xml, templates.xml, ReactStyledComponent.js, emmet.xml, React.xml, Default.xml, JavaScript.xml, _@user_Material Darker.icls, laf.xml, Mac OS X 10_5_ copy.xml, vcs.xml, editor.codeinsight.xml, keymap.xml, editor.xml, debugger.xml, updates.xml

---
+++ fileTemplates/React.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const $!NAME = () => {
+
+    return (
+    
+    )
+}
+
+export default $!NAME
---
+++ fileTemplates/ReactStyledComponent.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const Wrapper = styled.div`
+
+`
+
+const ${COMPONENT_NAME} = () => {
+  return (
+    <Wrapper>
+      
+    </Wrapper>
+  )
+}
+
+export default ${COMPONENT_NAME}
